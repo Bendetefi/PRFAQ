1ea197045c4a47bdbd6498957e7caf604dd77f95
Bendetefi <bendet.efi@gmail.com>
2024-11-07 23:55:30 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
+import "@/styles/globals.css"
 import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   )
 }
-
-export default MyApp
